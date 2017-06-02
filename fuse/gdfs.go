89851a0ca1c348d5a6ca41fd03f265cc67073ab6
Ke Yunjie <keyunjie96@gmail.com>
2017-06-02 06:14:25 -0700

    for pull

--- fuse/gdfs.go
+++ fuse/gdfs.go
@@ -37,7 +37,7 @@ func main() {
 	mountpoint := flag.Arg(0)
 
 	// init global
-	id2cont = make(map[string]Cont)
+	id2container = make(map[string]Cont)
 
 	// connect gdfs
 	utils.SetupProxyFromEnv()
@@ -79,8 +79,9 @@ func (f FS) Root() (fs.Node, error) {
 	return f.rootDir, nil
 }
 
-var id2cont map[string]Cont        // fileId->dir container
+var id2container map[string]Cont   // fileId->dir container
 var id2parentdir map[string]string // fileId->parentdir
+var id2content map[string][]byte   // fileId->content
 
 // Dir implements both Node and Handle for the root directory.
 type Cont struct {
@@ -100,8 +101,9 @@ func (d Dir) Attr(ctx context.Context, a *fuse.Attr) error {
 }
 
 func (d Dir) GetDirAll() {
-	_, ok := id2cont[d.fileId]
-	if !ok || !id2cont[d.fileId].hasUpdated {
+	fmt.Println("GetDirAll")
+	_, ok := id2container[d.fileId]
+	if !ok || !id2container[d.fileId].hasUpdated {
 		files, err := handler.List(d.fileId)
 		if err != nil {
 			log.Fatal(err)
@@ -123,7 +125,7 @@ func (d Dir) GetDirAll() {
 				Name:  file.Name,
 			})
 		}
-		id2cont[d.fileId] = Cont{
+		id2container[d.fileId] = Cont{
 			dirDirs:    dirDirs,
 			name2id:    name2id,
 			hasUpdated: true,
@@ -132,8 +134,9 @@ func (d Dir) GetDirAll() {
 }
 
 func (d Dir) Lookup(ctx context.Context, name string) (fs.Node, error) {
+	fmt.Println("Lookup")
 	d.GetDirAll()
-	id, ok := id2cont[d.fileId].name2id[name]
+	id, ok := id2container[d.fileId].name2id[name]
 	if ok {
 		if id[len(id)-1] == '0' {
 			// dir
@@ -150,14 +153,16 @@ func (d Dir) Lookup(ctx context.Context, name string) (fs.Node, error) {
 }
 
 func (d Dir) ReadDirAll(ctx context.Context) ([]fuse.Dirent, error) {
+	fmt.Println("ReadDirAll")
 	d.GetDirAll()
-	return id2cont[d.fileId].dirDirs, nil
+	return id2container[d.fileId].dirDirs, nil
 }
 
 func (d Dir) Mkdir(ctx context.Context, req *fuse.MkdirRequest) (fs.Node, error) {
+	fmt.Println("Mkdir")
 	d.GetDirAll()
 	name := req.Name
-	_, ok := id2cont[d.fileId].name2id[name]
+	_, ok := id2container[d.fileId].name2id[name]
 	// check existance
 	if ok {
 		return nil, fuse.EEXIST
@@ -169,23 +174,24 @@ func (d Dir) Mkdir(ctx context.Context, req *fuse.MkdirRequest) (fs.Node, error)
 		return nil, err
 	}
 	// local
-	id2cont[d.fileId].name2id[name] = newDir.Id + "0"
-	tmp := id2cont[d.fileId]
+	id2container[d.fileId].name2id[name] = newDir.Id + "0"
+	tmp := id2container[d.fileId]
 	tmp.dirDirs = append(tmp.dirDirs, fuse.Dirent{
 		Inode: utils.Str2u64(newDir.Id),
 		Type:  fuse.DT_Dir,
 		Name:  name,
 	})
-	id2cont[d.fileId] = tmp
+	id2container[d.fileId] = tmp
 	return Dir{
 		fileId: newDir.Id,
 	}, nil
 }
 
 func (d Dir) Remove(ctx context.Context, req *fuse.RemoveRequest) error {
+	fmt.Println("Remove")
 	d.GetDirAll()
 	name := req.Name
-	id, ok := id2cont[d.fileId].name2id[name]
+	id, ok := id2container[d.fileId].name2id[name]
 	// check existance
 	if !ok {
 		return fuse.ENOENT
@@ -193,16 +199,79 @@ func (d Dir) Remove(ctx context.Context, req *fuse.RemoveRequest) error {
 	// gd
 	handler.DeleteFile(id[:len(id)-1])
 	// local
-	delete(id2cont[d.fileId].name2id, name)
-	tmp := id2cont[d.fileId]
+	delete(id2container[d.fileId].name2id, name)
+	tmp := id2container[d.fileId]
 	for i, dir := range tmp.dirDirs {
 		if dir.Name == name {
 			tmp.dirDirs = append(tmp.dirDirs[:i], tmp.dirDirs[i+1:]...)
 			break
 		}
 	}
-	id2cont[d.fileId] = tmp
+	id2container[d.fileId] = tmp
 	return nil
+}
+
+// TODO: for dir rename create
+
+// func (d Dir) Link(ctx context.Context, req *fuse.LinkRequest, old fs.Node) (fs.Node, error) {
+// 	fmt.Println("Link")
+// 	return nil, nil
+// 	// d.GetDirAll()
+// 	// name := req.NewName
+// 	// oldId := old.fileId
+// 	// _, ok := id2container[d.fileId].name2id[name]
+// 	// // check existance
+// 	// if ok {
+// 	// 	return nil, fuse.EEXIST
+// 	// }
+// 	// // gd
+// 	// handler.LinkFile(oldId, d.fileId)
+
+// }
+
+func (d Dir) Rename(ctx context.Context, req *fuse.RenameRequest, _newDir fs.Node) error {
+	fmt.Println("Rename")
+	newDir, ok := _newDir.(Dir)
+	d.GetDirAll()
+	// check existance
+	_, ok = id2container[newDir.fileId].name2id[req.NewName]
+	if ok {
+		return fuse.EEXIST
+	}
+	id, ok := id2container[d.fileId].name2id[req.OldName]
+	if !ok {
+		return fuse.ENOENT
+	}
+	// gd
+	handler.MoveFile(id, d.fileId, newDir.fileId)
+	if req.OldName != req.NewName {
+		handler.RenameFile(id, req.NewName)
+	}
+	// local
+	// oldDir
+	delete(id2container[d.fileId].name2id, req.OldName)
+	tmp := id2container[d.fileId]
+	var dirent fuse.Dirent
+	for i, dir := range tmp.dirDirs {
+		if dir.Name == req.OldName {
+			dirent = tmp.dirDirs[i]
+			tmp.dirDirs = append(tmp.dirDirs[:i], tmp.dirDirs[i+1:]...)
+			break
+		}
+	}
+	id2container[d.fileId] = tmp
+	// newDir
+	id2container[newDir.fileId].name2id[req.NewName] = id
+	tmp = id2container[newDir.fileId]
+	tmp.dirDirs = append(tmp.dirDirs, dirent)
+	id2container[newDir.fileId] = tmp
+	return nil
+}
+
+func (d Dir) Create(ctx context.Context, req *fuse.CreateRequest, resp *fuse.CreateResponse) (fs.Node, fs.Handle, error) {
+	fmt.Println("Create")
+	d.GetDirAll()
+	return nil, nil, nil
 }
 
 // File implements both Node and Handle for the hello file.
@@ -213,14 +282,13 @@ type File struct {
 //const greeting = "hello, world\n"
 
 func (f File) Attr(ctx context.Context, a *fuse.Attr) error {
-	// TODO: use getfilesize
-	// var err errord
+	fmt.Println("Attr")
 	file, err := handler.GetFile(f.fileId)
 	if err != nil {
 		log.Fatal(err)
 		return err
 	}
-	a.Inode = 0 // let it get dynamic id automatic
+	a.Inode = 0 // let it get dynamic id automatic, WARNING
 	a.Mode = 0775
 	a.Size = uint64(file.Size)
 	fmt.Println(file.Size)
@@ -229,12 +297,17 @@ func (f File) Attr(ctx context.Context, a *fuse.Attr) error {
 }
 
 func (f File) ReadAll(ctx context.Context) ([]byte, error) {
+	fmt.Println("ReadAll")
+	content, ok := id2content[f.fileId]
+	if ok {
+		return content, nil
+	}
 	r, err := handler.OpenReader(f.fileId)
 	if err != nil {
 		log.Fatal(err)
 		return nil, err
 	}
-	content, err := ioutil.ReadAll(r)
+	content, err = ioutil.ReadAll(r)
 	if err != nil {
 		log.Fatal(err)
 		return nil, err
@@ -244,10 +317,19 @@ func (f File) ReadAll(ctx context.Context) ([]byte, error) {
 	return content, nil
 }
 
-// TODO: write file, create file
+// TODO: for file write
 
-// func (f file) Write(ctx context.Context, req *fuse.WriteRequest, resp *fuse.WriteResponse) error {
-// 	offset := req.Offset
-// 	fmt.Println(offset)
-
-// }
+func (f File) Write(ctx context.Context, req *fuse.WriteRequest, resp *fuse.WriteResponse) error {
+	fmt.Println("Write")
+	content, err := f.ReadAll(ctx)
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+	newLen := req.Offset + int64(len(req.Data))
+	if newLen := int(newLen); newLen > len(content) {
+		content = append(content, make([]byte, newLen-len(content))...)
+	}
+	resp.Size = copy(content[req.Offset:], req.Data)
+	return nil
+}
